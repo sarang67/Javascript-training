786c306f706391d37e7cf6d5562cb5be935e84fb
sarang <sarang>
2021-07-11 16:37:53 +0530

    js function execution context

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// 2 hoisting and ecection
+// 2 function execution context
+
+var name = "John";
+
+function first() {
+  var a = "Hello";
+  second();
+  var x = a + name;
+}
+
+function second() {
+  var b = "Hi";
+  third();
+  var z = b + name;
+}
+
+function third() {
+  var c = "Hey";
+  var y = c + name;
+}
+
+first();
+console.log("test");
